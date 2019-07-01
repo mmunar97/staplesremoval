5a4c4d19a856096cc04eaf7f2bf4b3334caab1dc
Marc Munar Covas <mmunar97@gmail.com>
2019-07-01 12:39:45 +0200

    Corrected some errors

--- examples/example_detect_infection.py
+++ examples/example_detect_infection.py
@@ -1,10 +1,13 @@
 from staplesremoval import detect_infection
 from staplesremoval import data
-import time
+from skimage import transform
+from skimage import io
+
 
 if __name__ == "__main__":
 
     image_infection = data.load_infection_example_image()
+    print(image_infection.shape)
     red_proportion_infection = detect_infection.detect_infection(image_infection)
     print("The image with infection has degree of infection {}".format(red_proportion_infection))
 
